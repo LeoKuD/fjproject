c2a6e915ff416eb8aa8a9373dc14d244bb32f561
A. Zhur <sanchesmrio1@gmail.com>
2022-02-06 00:25:50 +0300

    feat(admin-1): add create and edit user data modal forms

--- admin-4/index.js
+++ admin-4/index.js
@@ -1,15 +1,68 @@
-const testThemes = document.getElementById('testThemes');
-console.log(testThemes)
+let prevEditedTheme = null;
+let prevEditedThemeValue = null;
 
-testThemes.addEventListener('click', ({ target }) => {
-    const testItem = target.closest('.test-item');
-    console.log(testItem)
-    if (testItem) {
-        testItem.querySelector('.test-item__text').setAttribute( 'contenteditable',"true");
-        testItem.classList.add('edit');
-    }
+
+document.addEventListener('click', ({ target }) => {
+    const targetClassList = target.classList;
+   if (target.closest('#testThemes')) {
+    testThemeClichHandler(target)
+   } else if(target.closest('#detailList')) {
+    refreshThemesValues();
+    detailClickHandler(target);
     
+   }
+   refreshThemesValues();
 })
+
+function testThemeClichHandler(target) {
+    const testItem = target.closest('.test-item');
+    if (testItem) {
+        if (target.closest('.test-item__edit')) {
+            setThemeEditMode(testItem)
+        }
+    }
+}
+
+function setThemeEditMode(newTheme) {
+    if (prevEditedTheme) {
+        prevEditedTheme.classList.remove('edit');
+        prevEditedTheme.querySelector('.test-item__input').setAttribute('readonly', '');
+        prevEditedTheme.querySelector('.test-item__input').value = prevEditedThemeValue;
+    }
+    prevEditedTheme = newTheme;
+
+    if (newTheme) {
+        newTheme.classList.add('edit');
+        newTheme.querySelector('.test-item__input').removeAttribute('readonly');
+        newTheme.querySelector('.test-item__input').focus();
+        prevEditedThemeValue = newTheme.querySelector('.test-item__input').value;
+    }
+}
+
+function refreshThemesValues() {
+    setThemeEditMode(null);
+    prevEditedThemeValue = null;
+    
+}
+
+const prevQuestion = null;
+
+function detailClickHandler(target) {
+    if (target.closest('.question')) {
+        console.log('Yes')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 const activateAddTestButton = document.getElementById('activateAddTestButton');
 const addTestForm = document.getElementById('addTestForm');
